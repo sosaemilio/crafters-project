964a646ae9db3fc6454f5cbb6c54e632164efd71
alancrisanto <alanvcrisanto@gmail.com>
2024-01-26 12:32:38 -0500

    reset project styles

---
+++ src/components/filter/filter.tsx
@@ -0,0 +1,11 @@
+function Filter() {
+	return (
+		<div className="filter">
+			<div className="filter-title"></div>
+			<div className="filter-desc"></div>
+			<div className="filter-body"></div>
+		</div>
+	);
+}
+
+export default Filter;
