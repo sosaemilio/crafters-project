5970bf2336129f3c7973805dec7353f76e23a12d
acrisanto <alanvcrisanto@gmail.com>
2024-01-27 16:08:07 -0500

    delete: filter from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Filter from "@/components/filter/filter";
 
 export default function Home() {
 	return (
 		<main>
 			<h1>Hello from Home </h1>
-			<Filter />
 		</main>
 	);
 }
