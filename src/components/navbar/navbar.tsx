bea5453c13157b44ba602661c58adaa9ed872e83
acrisanto <alanvcrisanto@gmail.com>
2024-02-03 20:19:18 -0500

    refactor: navar component

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,26 +1,71 @@
-"use client"
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import NavLinks from './nav-links';
-import styles from './navbar.module.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
+import { Container, Button, Form, Nav, Navbar, Offcanvas, InputGroup } from "react-bootstrap";
+import Image from "next/image";
+import Link from "next/link";
+// import NavLinks from "./nav-links";
 
-export default function NavBar() {
-    return (
-      <div className={styles.navbar}>
-        <div className={styles.logo}>
-        <Link href="/">
-        <div className={styles.navimagediv}>
-            <Image src={require('./HandCraftedHavenLogo.JPG')} alt='something' width={369} height={89} className={styles.navimage} sizes="(max-width: 768px) 100vw, 33vw"></Image>
-        </div>
-      </Link>
-        </div>
-        <div className={styles.navbuttonsandtabsdiv}>
-            <Link className={styles.navbuttontwo} href='/signup' ><span className={styles.span}>Sign in</span></Link>
-            <Link className={styles.navbuttonone} href='/register' ><span className={styles.span}>Register</span></Link>
-            <NavLinks />
-        </div>
-      </div>
-    );
-  }
+function NavbarComponent() {
+	const expand = "lg"; // Definimos 'expand' como "sm" para mostrar solo un Navbar
+
+	return (
+		<>
+			<Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+				<Container fluid>
+					<Link href="#">
+						<Image src="/HandCraftedHavenLogo.jpg" alt="logo coolinary" width={150} height={45} />
+					</Link>
+					<Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+					<Navbar.Offcanvas
+						id={`offcanvasNavbar-expand-${expand}`}
+						aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+						placement="end"
+					>
+						<Offcanvas.Header closeButton>
+							<Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>Menu</Offcanvas.Title>
+						</Offcanvas.Header>
+						<Offcanvas.Body>
+							<Nav className="justify-content-end flex-grow-1 pe-3">
+								{/* <NavLinks /> */}
+								<Nav.Link href="#action1">Home</Nav.Link>
+								<Nav.Link href="#action2">Recipes</Nav.Link>
+								<Nav.Link href="#action2">About Us</Nav.Link>
+							</Nav>
+							<Form className="d-flex flex-column flex-lg-row gap-3 align-items-center">
+								<InputGroup>
+									<Form.Control
+										type="text"
+										placeholder="Search"
+										aria-label="Input group example"
+										aria-describedby="btnGroupAddon"
+									/>
+									<Button variant="outline-success">
+										<i className="bi bi-search"></i>
+									</Button>
+								</InputGroup>
+								<div className="d-flex w-100 justify-content-start justify-content-lg-end align-items-center  gap-2">
+									<Link href="#">
+										<Button variant="">
+											<i className="bi bi-cart2 fs-5"></i>
+										</Button>
+									</Link>
+									<Link href="/login">
+										<Button variant="outline-dark">Login</Button>
+									</Link>
+									<Link href="/signup">
+										<Button variant="success">Sign Up</Button>
+									</Link>
+								</div>
+							</Form>
+						</Offcanvas.Body>
+					</Navbar.Offcanvas>
+				</Container>
+			</Navbar>
+		</>
+	);
+}
+
+export default NavbarComponent;
