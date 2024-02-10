e401a8aeff23af7ce829e378e96cee1f9de0147f
almabenav2 <almabenavides02@gmail.com>
2024-02-10 13:15:42 -0500

    Cards info added

--- src/components/products-grid/card.tsx
+++ src/components/products-grid/card.tsx
@@ -1,28 +1,31 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./card.module.css";
 
-export default function Card({img, title, price}: {img: string, title: string, price: string}) {
+export default function Card({ product }) {
     return (
-      <div className={styles.card}>
         <div className={styles.card}>
-            <Image
-                src={img}
-                width={400}
-                height={400}
-                alt="Screenshots of the dashboard project showing desktop version"
-            />
+          <div className={styles.card}>
+              <Image
+                  src={product.image}
+                  width={400}
+                  height={400}
+                  alt="Screenshots of the dashboard project showing desktop version"
+              />
+          </div>
+          <h3>{product.product}</h3>
+          <div className={styles.cardBottom}>
+              <p>{`$ ${product.price}`}</p>
+              <Image
+                  src="/products/vector.png"
+                  width={20}
+                  height={18}
+                  alt="favorite product"
+              />
+          </div>
         </div>
-        <h3>{title}</h3>
-        <div className={styles.cardBottom}>
-            <p>{`$ ${price}`}</p>
-            <Image
-                src="/products/vector.png"
-                width={20}
-                height={18}
-                alt="favorite product"
-            />
-        </div>
-      </div>
+
     );
   }
   
