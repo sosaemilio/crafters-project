4e303f1b78587f06e79904cd765c1402c8c3d6b6
Emilio Sosa <emiliossosa85@gmail.com>
2024-02-11 02:23:15 -0400

    Fixed Product view

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,30 +2,31 @@ import { connectDB } from "@/lib/connection";
 import Product from "@/models/Products";
 
 import ProductPage from "@/components/ProductPage/ProductPage";
-import NewReviews from "@/components/Reviews/NewReviews";
-import Reviews from "@/components/Reviews/Reviews";
+import NewReviews from "@/components/reviews/NewReviews";
+import Reviews from "@/components/reviews/Reviews";
 
-
-async function loadProducts() {
-	connectDB();
-	const products = await Product.find()
-	return products
-}
-
-export default async function ProductDetailsPage() {
-    const products = await loadProducts()
+export default async function Page({ params }: { params: { id: string } }) {
+    connectDB();
+	const productData = await Product.findById(params.id);
 
     return (
         <div>
-            <ProductPage/>
+            <ProductPage 
+                image={productData.image} 
+                productName={productData.product} 
+                category={productData.category} 
+                price={productData.price} 
+                seller={productData.seller}
+            />
             <div className="p-4">
                 <NewReviews/>
                 <h1 className="text-secondary p-3 my-0">
                     Other Reviews
                 </h1>
-				{products.map(product => (<Reviews product={product} key={product._id}/>))}
+				{/* {products.map(product => (<Reviews product={product} key={product._id}/>))} */}
                 {/* <Reviews /> */}
-            </div>
+            </div> 
+           
         </div>
     )
 };
--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,26 +1,24 @@
 'use client';
-
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'next/navigation';
 import Image from 'next/image';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function ProductPage ({ product }) {
+export default function ProductPage ({ image, productName, category, price, seller }) {
 
-    const getProductDetails = async() => {
-        const res = await fetch(`api/products/${params.id}`)
-        const data = await res.json();
-        console.log(data);
-    }
-    const params = useParams()
-    useEffect(() => {
-        if (params.id){
-            getProductDetails()
-        }
-    },[])
+    
+    // const getProductDetails = async() => {
+    //     const res = await fetch(`api/products/${params.id}`)
+    //     const data = await res.json();
+    //     console.log(data);
+    // }
+    // const params = useParams()
+    // useEffect(() => {
+    //     if (params.id){
+    //         getProductDetails()
+    //     }
+    // },[])
 
-    const [activeImg, setActiveImage] = useState("/placeholder.png");
-    const [amount, setAmount] = useState(1);
+    // const [activeImg, setActiveImage] = useState("/placeholder.png");
+    // const [amount, setAmount] = useState(1);
 
 
     return (
@@ -28,7 +26,7 @@ export default function ProductPage ({ product }) {
             <div className='d-flex flex-column w-100 w-lg-50'>
                 <div className='w-auto'>
                     <Image
-                        src={activeImg}                    
+                        src={image}                    
                         width={400}
                         height={300}
                         className="img-fluid object-fit-cover" 
@@ -44,7 +42,6 @@ export default function ProductPage ({ product }) {
                             height={150}
                             className="img-fluid object-fit-cover pe-1 py-2" 
                             alt="Hero Banner" 
-                            onClick={() => setActiveImage("/placeholder.png")}
                         />
                     </div>
                     <div className='w-auto'>
@@ -54,25 +51,16 @@ export default function ProductPage ({ product }) {
                             height={150}
                             className="img-fluid object-fit-cover ps-1 py-2 " 
                             alt="Hero Banner" 
-                            onClick={() => setActiveImage("/placeholder1.png")}
                         />
                     </div>
                 </div>
             </div>
             <div className=' flex-column p-2 mx-lg-5 align-items-lg-center w-100 w-lg-50'>
                 <div>
-                    <h1 className='fw-bold'>{product?.product}</h1>
-                    <h4 className='fw-semibold'>$ {product?.price}</h4>
-                    <div className='d-flex flex-row w-100'>
-                        <div className='d-flex flex-row align-items-center'>
-                            <button type="button" className="btn btn-secondary py-2 px-3" onClick={() =>
-                                setAmount((prev) => prev - 1)}>-</button>
-                            <span className='py-2 px-2'>{amount}</span>
-                            <button type="button" className="btn btn-secondary py-2 px-3" onClick={() =>
-                                setAmount((prev) => prev + 1)}>+</button>
-                        </div>
-                        <button type="button" className="btn btn-secondary fw-semibold ms-3 py-2 px-4">Add to Cart</button>
-                    </div>
+                    <h1 className='fw-bold'>{productName}</h1>
+                    <h4 className='fw-semibold'>$ {price}</h4>
+                    <p>Seller: {seller}</p>
+                    <p>Product Category: {category}</p>
                 </div>
             </div>
         </div>
