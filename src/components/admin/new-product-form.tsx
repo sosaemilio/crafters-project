7d29517235fcb244a3f0f78567c3a9763b3e6420
Emilio Sosa <emiliossosa85@gmail.com>
2024-02-11 01:05:38 -0400

    Complete new product form

--- src/components/admin/new-product-form.tsx
+++ src/components/admin/new-product-form.tsx
@@ -4,50 +4,54 @@ import { createProduct } from "@/lib/action";
 export default function NewProductForm() {
     return(
     <form className={style.newProductForm} action={createProduct}>
-    {/* Product Name  */}
-      <label htmlFor="product">
-        Product
-      </label>
-      <input 
-        id="product" 
-        name="product" 
-        type="text"
-        className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-      />
-    
-    {/* Price  */}
-      <label htmlFor="price">
-        Price
-      </label>
-      <input
-        id="price"
-        name="price"
-        type="number"
-        step="0.01"
-        placeholder="Enter USD amount"
-        className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-        required
-      />
-      {/* Category */}
-      <label htmlFor="category">
-        Category
-      </label>
-      <input 
-        id="category" 
-        name="category" 
-        type="text"
-        className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-      />
-      {/* Seller Name */}
-      <label htmlFor="seller">
-        Seller Name
-      </label>
-      <input 
-        id="seller" 
-        name="seller" 
-        type="text"
-        className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-      />
-      <input type="submit" value="Send Request"/>
+      {/* Product Name  */}
+        <label htmlFor="product">
+          Product
+        </label>
+        <input 
+          id="product" 
+          name="product" 
+          type="text"
+          className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+        />
+      
+      {/* Price  */}
+        <label htmlFor="price">
+          Price
+        </label>
+        <input
+          id="price"
+          name="price"
+          type="number"
+          step="0.01"
+          placeholder="Enter USD amount"
+          className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+          required
+        />
+        {/* Category */}
+        <label htmlFor="category">
+          Category
+        </label>
+        <input 
+          id="category" 
+          name="category" 
+          type="text"
+          className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+        />
+        {/* Seller Name */}
+        <label htmlFor="seller">
+          Seller Name
+        </label>
+        <input 
+          id="seller" 
+          name="seller" 
+          type="text"
+          className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+        />
+        {/* Images */}
+        <label htmlFor="image">Add a Product Image</label>
+        <input type="file" id="image" name="image" accept="image/png, image/jpeg" />
+        
+        <input type="submit" value="Send Request" className={style.button}/>
     </form>);
 }
